4c723bde338684c54fffbe2cf47546127db5885c
Affan Ahmed <affanasrar809@gmail.com>
2026-05-06 10:59:46 +0500

    Update introduction and social links in intro.tsx

--- components/intro.tsx
+++ components/intro.tsx
@@ -62,11 +62,10 @@ export default function Intro() {
         initial={{ opacity: 0, y: 100 }}
         animate={{ opacity: 1, y: 0 }}
       >
-        <span className="font-bold">Hello, I'm Ricardo.</span> I'm a{" "}
-        <span className="font-bold">full-stack developer</span> with{" "}
-        <span className="font-bold">8 years</span> of experience. I enjoy
-        building <span className="italic">sites & apps</span>. My focus is{" "}
-        <span className="underline">React (Next.js)</span>.
+        <span className="font-bold">Hello, I'm Affan Ahmed.</span> I'm a{" "}
+        <span className="font-bold">full-stack developer</span> focused on learning 
+        and building <span className="italic">real-world web applications</span>. 
+        My main stack is <span className="underline">React (Next.js)</span>.
       </motion.h1>
 
       <motion.div
@@ -100,7 +99,7 @@ export default function Intro() {
 
         <a
           className="bg-white p-4 text-gray-700 hover:text-gray-950 flex items-center gap-2 rounded-full focus:scale-[1.15] hover:scale-[1.15] active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10 dark:text-white/60"
-          href="https://linkedin.com"
+          href="www.linkedin.com/in/affan-ahmed809"
           target="_blank"
         >
           <BsLinkedin />
@@ -108,7 +107,7 @@ export default function Intro() {
 
         <a
           className="bg-white p-4 text-gray-700 flex items-center gap-2 text-[1.35rem] rounded-full focus:scale-[1.15] hover:scale-[1.15] hover:text-gray-950 active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10 dark:text-white/60"
-          href="https://github.com"
+          href="https://github.com/affanasrar809-cyber"
           target="_blank"
         >
           <FaGithubSquare />
